00cd61eb7264d6f5223480c01b4daad71e82cae5
destro1382 <destro1382@gmail.com>
2026-03-31 16:30:31 -0400

    test

--- village.py
+++ village.py
@@ -15,3 +15,27 @@ village_square = Room(
     [],
     []
 )
+
+blacksmith = Room(
+    "Blacksmith",
+    "You find yourself at the entryway of a dark and foreboding keep.  As the sky darkens overhead, the ominous feeling in your breast grows dramatically.\nDespite this feeling, you know that your quest lies within these walls. You must enter and face what is within.\nThe only way forward is through the grand doors ahead to the South.",
+    {"west": "village square"},
+    [],
+    []
+)
+
+inn = Room(
+    "inn",
+    "You find yourself at the entryway of a dark and foreboding keep.  As the sky darkens overhead, the ominous feeling in your breast grows dramatically.\nDespite this feeling, you know that your quest lies within these walls. You must enter and face what is within.\nThe only way forward is through the grand doors ahead to the South.",
+    {"east": "village square"},
+    [],
+    []
+)
+
+northern_village = Room(
+    "Northern Village",
+    "You find yourself at the entryway of a dark and foreboding keep.  As the sky darkens overhead, the ominous feeling in your breast grows dramatically.\nDespite this feeling, you know that your quest lies within these walls. You must enter and face what is within.\nThe only way forward is through the grand doors ahead to the South.",
+    {"south": "village square"},
+    [],
+    []
+)
